ac8c077cb0b5165ecbbe81f207c6156c00ab4549
souvc <291117974@qq.com>
2016-10-14 09:51:40 +0800

    配置springboot支持jsp

---
+++ Chapter3-1-7/src/test/java/com/didispace/ApplicationTests.java
@@ -0,0 +1,22 @@
+package com.didispace;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ApplicationTests {
+
+	private static final Log log = LogFactory.getLog(ApplicationTests.class);
+
+	@Test
+	public void test1() throws Exception {
+		
+		
+	}
+
+}
